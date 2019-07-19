7182068fbd8c89ce4e50d217755948760b019b29
Hao Wu <hawu@pivotal.io>
2019-07-19 19:07:41 +0800

    make code cleaner

--- R/db.gpapply.R
+++ R/db.gpapply.R
@@ -5,19 +5,9 @@
         return (signature)
     if (is.function(signature))
         return (simplify.signature(signature()))
-    if (is.db.data.frame(signature)) {
-        map.type <- function(y) {
-            switch(typeof(y),
-                double  = NZ.DOUBLE,
-                integer = NZ.INT32,
-                character = list(NZ.VARIABLE, 256),
-                logical = NZ.BOOL
-            )
-        }
-        df <- as.db.data.frame(signature)
-        res <- lapply(names(df), map.type)
-        return(res)
-    }
+    if (is.db.data.frame(signature))
+        stop("type of signature is db.data.frame, not supported now")
+
     stop(paste("invalid signature:", signature))
 }
 .distribute.str <- function(distributeOn) {
@@ -35,86 +25,89 @@
         stop("invalid distributed value")
     }
 }
+.extract.param.list <- function(param_list) {
+    if (is.null(param_list) || length(param_list)==0)
+        return ("")
+    arg_str_array <- strsplit(deparse(param_list), ", .Names = ")[[1]]
+    print(length(arg_str_array))
+    if (length(arg_str_array) == 1)
+    {
+        listStr <- substr(arg_str_array[1], 6, nchar(arg_str_array[1]) - 1)
+    }
+    else if (length(arg_str_array) == 2)
+    {
+        listStr <- substr(arg_str_array[1], 16, nchar(arg_str_array[1]) - 1)
+    }
+    else
+    {
+        stop("The functon input argument must not inlcude '.Names'")
+    }
+}
 
+db.gpapply <- function(X, MARGIN=NULL, FUN = NULL, output.name=NULL, output.signature=NULL,
+        clear.existing=FALSE, case.sensitive=FALSE,output.distributeOn=NULL, language="plcontainer", ...)
+{
+    if (!is.function(FUN))
+        stop("FUN must be a function")
+    randomName <- getRandomNameList()[1]
 
-db.gpapply <- function(X, MARGIN, FUN = NULL, output.name=NULL, output.signature=NULL,
-		clear.existing=FALSE, case.sensitive=FALSE,output.distributeOn=NULL, language="plcontainer", ...)
-{	
-	args <- list(...)
-
-	randomName <- getRandomNameList()[1]
-
-	typeName <- sprintf("gptype_%s", randomName)
-	if (!is.null(output.signature))
-	{
-		typelist_str <- sprintf("CREATE TYPE %s AS (\n", typeName)
-		typelist <- .simplify.signature(output.signature)
+    typeName <- sprintf("gptype_%s", randomName)
+    if (!is.null(output.signature))
+    {
+        typelist_str <- sprintf("CREATE TYPE %s AS (\n", typeName)
+        typelist <- .simplify.signature(output.signature)
         fieldStr <- paste(names(typelist), typelist, sep=" ", collapse=",\n")
-		typelist_str <- paste(typelist_str, fieldStr, "\n);", sep="")
-		print(typelist_str)
-		db.q(typelist_str)
-	}
-	
-	# generate function parameter str
-	ar <- attributes(X)
-    listStr <- ""
+        typelist_str <- paste(typelist_str, fieldStr, "\n);", sep="")
+        print(typelist_str)
+        db.q(typelist_str)
+    }
+
+    # generate function parameter str
+    ar <- attributes(X)
     param_list_str <- ""
-	local_data_frame_str <- ""
-	relation_name <- ar$.content
+    local_data_frame_str <- ""
+    relation_name <- ar$.content
 
     local_data_frame_str <- paste(ar$.col.name, ar$.col.name, sep="=", collapse=", ")
     param_list_str <- paste(ar$.col.name, sep=", ")
-	print(param_list_str)
+    print(param_list_str)
 
     # extract parameter list from gpapply
-    if (length(args)>0) {
-        arg_str_array <- strsplit(deparse(args), ", .Names = ")[[1]]
-        print(length(arg_str_array))
-        if (length(arg_str_array) == 1)
-        {
-            listStr = substr(arg_str_array[1], 6, nchar(arg_str_array[1]) - 1)
-        }
-        else if (length(arg_str_array) == 2)
-        {
-            listStr = substr(arg_str_array[1], 16, nchar(arg_str_array[1]) - 1)
-        }
-        else
-        {
-            stop("The functon input argument must not inlcude '.Names'")
-        }
+    listStr <- .extract.param.list(list(...))
+    if (nchar(listStr)>0)
         listStr <- paste(", ", listStr, sep="")
+    print(paste('listStr=', listStr))
+
+    funName <- paste("gprfunc_", randomName, sep="")
+    funBody <- paste("# container:  plc_r_shared\ngplocalf <- ", paste(deparse(FUN), collapse="\n"))
+    localdf <- sprintf("df <- data.frame(%s)\n", local_data_frame_str)
+    localcall <- sprintf("do.call(gplocalf, list(df %s))", listStr);
+
+    createStmt <- sprintf("CREATE FUNCTION %s (%s) RETURNS SETOF %s AS $$ %s\n %s\ return(%s)\n $$ LANGUAGE '%s';",
+    funName, param_list_str, typeName, funBody, localdf, localcall, language);
+
+    print(createStmt)
+    db.q(createStmt)
+
+    if (is.null(output.name))
+    {
+        query <- sprintf("WITH gpdbtmpa AS (SELECT (%s(%s)) AS gpdbtmpb FROM (SELECT %s FROM %s) tmptbl) SELECT (gpdbtmpb::%s).* FROM gpdbtmpa;",
+                funName, param_list_str, param_list_str, relation_name, typeName)
     }
+    else
+    {
+        query <- sprintf("CREATE TABLE %s AS WITH gpdbtmpa AS (SELECT (%s(%s)) AS gpdbtmpb FROM (SELECT %s FROM %s) tmptbl) SELECT (gpdbtmpb::%s).* FROM gpdbtmpa %s;",
+                output.name, funName, param_list_str, param_list_str, relation_name, typeName, .distribute.key(distributeOn))
+    }
+    print(query)
 
-	funName <- paste("gprfunc_", randomName, sep="")
-	funBody <- paste("# container:  plc_r_shared\ngplocalf <- ", paste(deparse(FUN), collapse="\n"))
-	localdf <- sprintf("df <- data.frame(%s)\n", local_data_frame_str)
-	localcall <- sprintf("do.call(gplocalf, list(df %s))", listStr);
+    results <- db.q(query, nrows = NULL)
 
-	createStmt <- sprintf("CREATE FUNCTION %s (%s) RETURNS SETOF %s AS $$ %s\n %s\ return(%s)\n $$ LANGUAGE '%s';",
-	funName, param_list_str, typeName, funBody, localdf, localcall, language);
+    cleanString <- sprintf("DROP TYPE %s CASCADE;",typeName)
 
-	#print(createStmt)
-	db.q(createStmt)
+    print(cleanString)
+    db.q(cleanString)
 
-	if (is.null(output.name))
-	{
-		query <- sprintf("WITH a AS (SELECT (%s(%s)) AS b FROM (SELECT %s FROM %s) tmptbl) SELECT (b::%s).* FROM a;",
-				funName, param_list_str, param_list_str, relation_name, typeName)
-	}
-	else
-	{
-		query <- sprintf("CREATE TABLE %s AS WITH a AS (SELECT (%s(%s)) AS b FROM (SELECT %s FROM %s) tmptbl) SELECT (b::%s).* FROM a %s;",
-				output.name, funName, param_list_str, param_list_str, relation_name, typeName, .distribute.key(distributeOn))
-	}
-	print(query)
-
-	results <- db.q(query, nrows = NULL)
-
-	cleanString <- sprintf("DROP TYPE %s CASCADE;",typeName)
-
-	print(cleanString)
-	db.q(cleanString)
-
-	return(results)
+    return(results)
 
 }
