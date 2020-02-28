ba0847ba9d0bf021144f5e6d4bc3267cea3a57a0
Haozhou Wang <hawang@pivotal.io>
2020-02-28 18:15:23 +0800

    add release pipeline for GreenplumR

---
+++ concourse/scripts/greenplumr_release.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#------------------------------------------------------------------------------
+#
+# Copyright (c) 2017-Present Pivotal Software, Inc
+#
+#------------------------------------------------------------------------------
+
+set -exo pipefail
+
+CWDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+TOP_DIR=${CWDIR}/../../../
+
+release_greenplumr() {
+
+  # Get GreenplumR version
+  pushd GreenplumR_src
+  if git describe --tags >/dev/null 2>&1 ; then
+      echo "git describe failed" || exit 1
+  fi
+  GREENPLUMR_VERSION=$(git describe --tags | awk -F. '{printf("%d.%d", $1, $2)}')
+  GREENPLUMR_RELEASE=$(git describe --tags | awk -F. '{print $3}')
+  popd
+
+  # release GreenplumR
+  mkdir -p release_greenplumr
+  tar zcf ${TOP_DIR}/greenplumR/tar.gz ${TOP_DIR}/GreenplumR_src
+  cp greenplumR.tar.gz release_greenplumr/greenplumR-${GREENPLUMR_VERSION}.${GREENPLUMR_RELEASE}-gp6.tar.gz
+}
+
+release_greenplumr
