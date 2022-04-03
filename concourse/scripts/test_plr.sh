a796b2b7e641ec464e973dc4e5e229d716ff835a
yihong0618 <zouzou0208@gmail.com>
2022-04-03 12:29:36 +0800

    fix: no ip command

--- concourse/scripts/test_plr.sh
+++ concourse/scripts/test_plr.sh
@@ -98,7 +98,7 @@ function install_libraries() {
     ubuntu)
       apt update
       DEBIAN_FRONTEND=noninteractive apt install -y r-base libpq-dev \
-          openssh-client openssh-server iputils-ping
+          openssh-client openssh-server iputils-ping net-tools
       R=R
       Rscript=Rscript
       ;;
